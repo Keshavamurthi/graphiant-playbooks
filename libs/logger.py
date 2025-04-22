d321b5dd9c6be51fd8d4b5ef29787e53246af9b6
keshavamurthi <keshavamurthi@graphiant.com>
2025-04-22 12:08:30 -0700

    TE-3538:Creating Logs directory

--- libs/logger.py
+++ libs/logger.py
@@ -9,8 +9,10 @@ def setup_logger(level=logging.INFO):
     # Prevent duplicate handlers in Jupyter/IDE environments
     if not logger.handlers:
         cwd = path.cwd()
+        logs_dir = cwd / 'logs'
+        logs_dir.mkdir(parents=True, exist_ok=True)
         timestamp_str = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-        log_file = cwd / 'logs' / f'log_{timestamp_str}.log'
+        log_file = logs_dir / f'log_{timestamp_str}.log'
         
         # File handler
         fh = logging.FileHandler(log_file)
